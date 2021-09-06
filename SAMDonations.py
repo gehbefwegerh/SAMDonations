d1074f461fe08477db90cd293b2216b340efe322
gehbefwegerh <60407900+gehbefwegerh@users.noreply.github.com>
2021-09-07 00:09:53 +0100

    slightly more pep8 compliant

--- SAMDonations.py
+++ SAMDonations.py
@@ -1,8 +1,9 @@
 #! python3
 """SAMDonations.py - Retrieves donations from streamlabs, uses SAM to
 read out the donator name and donation amount, then splits up the
-donation message into 100 chr strings to bypass SAM chr limit and uses
-it to read them out sequentially.
+donation message into individual word strings to bypass SAM chr limit
+and uses it to read them out sequentially.
+
 Note: For this code to work as-is, you need to have Sebastian Macke's
 port of the SAM software on your PC and either set in the Path system
 environment variable or in the same folder as this script."""
@@ -14,18 +15,19 @@ import time
 import requests
 import json
 
-CURRENCIES = {"AUD":"Australian Dollars", "BRL":"Brazilian Reais",
-              "CAD":"Canadian Dollars", "CZK":"Czech Korunas", 
-              "DKK":"Danish Kroner", "EUR":"Euros", "HKD":"Hong Kong Dollars",
-              "ILS":"Israeli New Sheqels", "MYR":"Malaysian Ringgit",
-              "MXN":"Mexican Pesos", "NOK":"Norwegian Kroner",
-              "NZD":"New Zealand Dollar", "PHP":"Philippine Pesos",
-              "PLN":"Polish Zloty", "GBP":"Pound Sterling",
-              "RUB":"Russian Rubles", "SGD":"Singapore Dollars",
-              "SEK":"Swedish Kronur", "CHF":"Swiss Francs", "THB":"Thai Baht",
-              "TRY":"Turkish Liras", "USD":"U.S. Dollars"}
+CURRENCIES = {"AUD": "Australian Dollars", "BRL": "Brazilian Reais",
+              "CAD": "Canadian Dollars", "CZK": "Czech Korunas",
+              "DKK": "Danish Kroner", "EUR": "Euros",
+              "HKD": "Hong Kong Dollars", "ILS": "Israeli New Sheqels",
+              "MYR": "Malaysian Ringgit", "MXN": "Mexican Pesos",
+              "NOK": "Norwegian Kroner", "NZD": "New Zealand Dollar",
+              "PHP": "Philippine Pesos", "PLN": "Polish Zloty",
+              "GBP": "Pound Sterling", "RUB": "Russian Rubles",
+              "SGD": "Singapore Dollars", "SEK": "Swedish Kronur",
+              "CHF": "Swiss Francs", "THB": "Thai Baht",
+              "TRY": "Turkish Liras", "USD": "U.S. Dollars"}
 # Change this to the path of your access token file.
-# You shouldn't really use a plaintext file, but I'm lazy. 
+# You shouldn't really use a plaintext file, but I'm lazy.
 # Would be better to instead store it somewhere secure and have input()
 # prompt you for it
 TOKEN_FILE = open("accessToken.txt")
@@ -33,35 +35,35 @@ ACCESS_TOKEN = TOKEN_FILE.read()
 TOKEN_FILE.close()
 URL = "https://streamlabs.com/api/v1.0/donations"
 HEADERS = {"Accept": "application/json"}
-# Gets donations in json, converts it to a python dict, then gets the 
-# ID of the most recent donation and converts it to int. It's all we 
+# Gets donations in json, converts it to a python dict, then gets the
+# ID of the most recent donation and converts it to int. It's all we
 # need.
-START_DONATION_ID = int(json.loads(requests.request("GET", URL, headers=HEADERS,
-                  params={"access_token":ACCESS_TOKEN}).text
-                  )["data"][0]["donation_id"])
+START_DONATION_ID = int(json.loads(requests.request("GET", URL,
+                        headers=HEADERS, params={"access_token": ACCESS_TOKEN}
+                                   ).text)["data"][0]["donation_id"])
 # delay due to testing status, bug me to fix it if this becomes public
 time.sleep(13)
+
 
 def mainLoop(ID):
     while True:
         # resets start number, then retrieves new donations
-        start = 0
         newDonations = json.loads(requests.request("GET", URL, headers=HEADERS,
-                                  params={"access_token":ACCESS_TOKEN,
-                                          "after":ID}).text)["data"]
+                                  params={"access_token": ACCESS_TOKEN,
+                                          "after": ID}).text)["data"]
         # if there are any new donations, reads the oldest and sets the
         # new start ID
         if newDonations:
             currentDonation = newDonations[-1]
             ID = currentDonation["donation_id"]
-            
+
             # Reads out the name, amount, and currency of the donation.
-            os.system(f"cmd /c sam {currentDonation['name']} donated " + 
-                       f"{str(float(currentDonation['amount']))} " +
-                       f"{CURRENCIES[currentDonation['currency']]}")
+            os.system(f"cmd /c sam {currentDonation['name']} donated " +
+                      f"{str(float(currentDonation['amount']))} " +
+                      f"{CURRENCIES[currentDonation['currency']]}")
             # slight pause before reading the message
             time.sleep(0.5)
-            
+
             message = currentDonation["message"]
             if message:
                 mesWords = message.split(" ")
